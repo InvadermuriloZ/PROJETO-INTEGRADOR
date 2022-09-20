ee574c3363e932a153cbc97d87671e1c4c9a0e87
Vanessa Morais <101489085+Nessa-Morais@users.noreply.github.com>
2022-09-20 08:38:10 -0300

    PHP e BD - versão 1.2

--- loginUsuario.php
+++ loginUsuario.php
@@ -47,7 +47,15 @@
         </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
+    </body>
+    </html>
+    <?php
 
-
+    }
+    else{
+        echo "
+            <p> Esta é uma página de tratamento de dados. </p>
+            <p> Clique <a href='listarUsuario.php'>aqui</a> para selecionar um Usuário. </p>
+        ";   
+    }
+?>
--- updateUsuario.php
+++ updateUsuario.php
@@ -3,17 +3,15 @@
     include "conexao.php";
 
     if(isset($_POST['login'])){
-        //entrada - coletar os dados do formulário
+
         $login = trim($_POST['login']);
         $nome = trim($_POST['nome']);
         $email = trim($_POST['email']);
         
 
-        //processamento - incluir no banco de dados
         $sql = "update usuarios set nome = '$nome', email = '$email' where login = '$login'";
         $alterar = mysqli_query($conexao,$sql);
 
-        //saida - feedback ao usuário
         if($alterar){
             echo "
                 <script>
