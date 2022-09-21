0989dd1a6094d91fce1f1266e44fa9051b1a4e1f
Rafael Araujo <108342479+RafaelAra@users.noreply.github.com>
2022-09-21 09:46:24 -0300

    Delete Imovel directory

--- Imovel/logoutImovel.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    session_start(); //inicia a sessão
-    $_SESSION = array(); //carrega o array de dados
-    session_destroy(); //destroi todas as sessôes iniciadas
-    header('location: \rafael01\Imovel/loginImovel.php'); // redireciona para a área de login
-
-?>
